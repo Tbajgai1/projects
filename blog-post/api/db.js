f8f8067b69f2ec05b9f186c7c5ce138a4850c048
Tbajgai1 <79878751+Tbajgai1@users.noreply.github.com>
2023-11-16 12:04:00 -0700

    Update db.js

--- blog-post/api/db.js
+++ blog-post/api/db.js
@@ -1,9 +1,1 @@
-import mysql from "mysql";
-// import mysql from 'mysql2/promise';
 
-export const db = mysql.createConnection({
-    host:"localhost",
-    user:"root",
-    password:"abcdef123@",
-    database:"blog"
-})
